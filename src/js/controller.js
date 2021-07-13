26252ee34569cc9094ea6307e25ae2c5b57e2255
Vitaliy Gosteev <vitaliygosteev@gmail.com>
2021-07-13 21:19:45 +0500

    Set up API for loading Recipes

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,6 @@
+const { title } = require("process");
+const { async } = require("q");
+
 const recipeContainer = document.querySelector(".recipe");
 
 const timeout = function (s) {
@@ -7,3 +10,31 @@ const timeout = function (s) {
     }, s * 1000);
   });
 };
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
